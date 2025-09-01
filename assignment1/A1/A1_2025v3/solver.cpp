2e456e8802656ebc9b8dc70d6ccc8540555d918c
Shreyash Mohanty <shreyashmohanty689@gmail.com>
2025-09-01 15:42:46 -0700

    heuristic function added

--- assignment1/A1/A1_2025v3/solver.cpp
+++ assignment1/A1/A1_2025v3/solver.cpp
@@ -11,38 +11,69 @@ using namespace std;
  * plan to demonstrate how to build the Solution object. 
  * * TODO: REPLACE THIS ENTIRE FUNCTION WITH YOUR ALGORITHM.
  */
+
+double h_val(Solution &sol,const ProblemData &problem){
+    double tot = 0.0;
+    for(auto plan:sol){
+        for(auto trip:plan.trips){
+            double dist = 0.0;
+            for(int i = 0;i<trip.drops.size();i++){
+                if(i == 0){
+                    dist+=distance(problem.villages[problem.helicopters[plan.helicopter_id].home_city_id].coords,problem.villages[trip.drops[i].village_id].coords);
+                }
+                else{
+                    dist+=distance(problem.villages[trip.drops[i-1].village_id].coords,problem.villages[trip.drops[i].village_id].coords);
+                }
+            }
+            dist+=distance(problem.villages[trip.drops[trip.drops.size()-1].village_id].coords,problem.villages[problem.helicopters[plan.helicopter_id].home_city_id].coords);
+            double cost = problem.helicopters[plan.helicopter_id].fixed_cost + problem.helicopters[plan.helicopter_id].alpha*dist;
+            double val = 0.0;
+            for(auto drop:trip.drops){
+                if(9*problem.villages[drop.village_id].population<drop.perishable_food+drop.dry_food ||problem.villages[drop.village_id].population<drop.other_supplies){
+                    continue;
+                }
+                val+=problem.packages[0].value*drop.dry_food + problem.packages[1].value*drop.perishable_food + problem.packages[2].value*drop.other_supplies;
+            }
+            tot+=val-cost;
+        }  
+    }
+    return tot;
+}
 Solution solve(const ProblemData& problem) {
     cout << "Starting solver..." << endl;
 
     Solution solution;
+    auto start = chrono::high_resolution_clock::now();
+    while(true){
 
+    }
     // --- START OF PLACEHOLDER LOGIC ---
     // This is a naive example: send each helicopter on one trip to the first village.
     // This will definitely violate constraints but shows the structure.
     
-    for (const auto& helicopter : problem.helicopters) {
-        HelicopterPlan plan;
-        plan.helicopter_id = helicopter.id;
+    // for (const auto& helicopter : problem.helicopters) {
+    //     HelicopterPlan plan;
+    //     plan.helicopter_id = helicopter.id;
 
-        if (!problem.villages.empty()) {
-            Trip trip;
-            // Pickup 1 of each package type
-            trip.dry_food_pickup = 1;
-            trip.perishable_food_pickup = 1;
-            trip.other_supplies_pickup = 1;
+    //     if (!problem.villages.empty()) {
+    //         Trip trip;
+    //         // Pickup 1 of each package type
+    //         trip.dry_food_pickup = 1;
+    //         trip.perishable_food_pickup = 1;
+    //         trip.other_supplies_pickup = 1;
 
-            // Drop them at the first village
-            Drop drop;
-            drop.village_id = problem.villages[0].id;
-            drop.dry_food = 1;
-            drop.perishable_food = 1;
-            drop.other_supplies = 1;
+    //         // Drop them at the first village
+    //         Drop drop;
+    //         drop.village_id = problem.villages[0].id;
+    //         drop.dry_food = 1;
+    //         drop.perishable_food = 1;
+    //         drop.other_supplies = 1;
 
-            trip.drops.push_back(drop);
-            plan.trips.push_back(trip);
-        }
-        solution.push_back(plan);
-    }
+    //         trip.drops.push_back(drop);
+    //         plan.trips.push_back(trip);
+    //     }
+    //     solution.push_back(plan);
+    // }
     
     // --- END OF PLACEHOLDER LOGIC ---
 
