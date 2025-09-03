297e88d2f1d2677cf5d43a3e2f27f33fc36f6160
Shreyash Mohanty <shreyashmohanty689@gmail.com>
2025-09-03 12:31:49 -0700

    completed neighbourhood function

--- assignment1/A1/A1_2025v3/solver.cpp
+++ assignment1/A1/A1_2025v3/solver.cpp
@@ -4,6 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #include <random>
+#define pb push_back
 // You can add any helper functions or classes you need here.
 
 /**
@@ -41,38 +42,83 @@ double h_val(Solution &sol,const ProblemData &problem){
     return tot;
 }
 
-vector<pair<Solution,int>> generate_neighbours(Solution &sol, const ProblemData & prob , int d_max){
-    vector<Solution>genn;
+vector<pair<Solution,ProblemData>> generate_neighbours_add(Solution &sol, const ProblemData & prob){
+    vector<pair<Solution,ProblemData>>genn;
     for(auto plan : sol){
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int>distr(0,plan.trips.size()-1);
+        int ind = distr(gen);
+        auto trip = &plan.trips[ind];
         Solution neigh = sol;
-        for(auto &trip:plan.trips){
-            set<int>st;
-            for(int i = 0;i<prob.villages.size();i++){
-                st.insert(i);
+        set<int>st;
+        ProblemData prob1 = prob;
+        for(int i = 0;i<prob.villages.size();i++){
+            st.insert(i);
+        }
+        for(auto drop:trip->drops){
+            st.erase(drop.village_id);
+        }
+        uniform_int_distribution<int> dist(0, (int)st.size()-1);
+        auto it = st.begin();
+        advance(it, dist(gen));
+        int id = *it;
+        if(trip->drops.size() == 0){
+            if(2*distance(prob.cities[prob.helicopters[plan.helicopter_id].home_city_id],prob.villages[id].coords)<=prob1.d_max&&distance(prob.cities[prob.helicopters[plan.helicopter_id].home_city_id],prob.villages[id].coords)<=prob.helicopters[plan.helicopter_id].distance_capacity){
+                uniform_real_distribution<double> dist1(0.0, 1.0);
+                double ratio = dist1(gen);
+                uniform_real_distribution<double> dist2(1.0-ratio,1.0);
+                double r2 = dist2(gen);
+                uniform_int_distribution<int> dist3(0,prob.helicopters[plan.helicopter_id].weight_capacity-trip->dry_food_pickup-trip->other_supplies_pickup-trip->perishable_food_pickup);
+                int morefood = dist3(gen);
+                int d = ratio*morefood;
+                int p = r2*morefood;
+                int o = morefood-d-p;
+                Drop newdrop;
+                newdrop.dry_food = d;
+                newdrop.other_supplies = o;
+                newdrop.perishable_food = p;
+                newdrop.village_id = id;
+                trip->dry_food_pickup+=d;
+                trip->other_supplies_pickup+=o;
+                trip->perishable_food_pickup+=p;
+                trip->drops.push_back(newdrop);
+                prob1.d_max-=2*distance(prob.cities[prob.helicopters[plan.helicopter_id].home_city_id],prob.villages[id].coords);
+                genn.pb({neigh,prob1});
             }
-            for(auto drop:trip.drops){
-                st.erase(drop.village_id);
-            }
-            random_device rd;
-            mt19937 gen(rd());
-            uniform_int_distribution<int> dist(0,st.size());
-            int id = dist(gen);
-            int c = 0;
-            while(c<id){
-                st.erase(st.begin());
-                c++;
-            }
-            id = *st.begin();
-            if(trip.drops.size() == 0){
-                if(2*distance(prob.cities[prob.helicopters[plan.helicopter_id].home_city_id],prob.villages[id].coords)<=d_max){
-                    uniform_real_distribution<double> dist1(0.0, 1.0);
-                    double ratio = dist1(gen);
-                    uniform_int_distribution<int> dist1(0,prob.helicopters[plan.helicopter_id].weight_capacity);
-                }
+        }
+        else{
+            uniform_int_distribution<int>dist2(0,trip->drops.size()-1);
+            int bet = dist2(gen);
+            double d1 = distance(prob.villages[trip->drops[bet].village_id].coords,prob.villages[id].coords);
+            double d2 = distance(prob.villages[trip->drops[(bet+1)%trip->drops.size()].village_id].coords,prob.villages[id].coords);
+            if(d1+d2<=prob1.d_max&&d1<=prob.helicopters[plan.helicopter_id].distance_capacity&&d2<=prob.helicopters[plan.helicopter_id].distance_capacity){
+                uniform_real_distribution<double> dist1(0.0, 1.0);
+                double ratio = dist1(gen);
+                uniform_real_distribution<double> dist2(1.0-ratio,1.0);
+                double r2 = dist2(gen);
+                uniform_int_distribution<int> dist3(0,prob.helicopters[plan.helicopter_id].weight_capacity-trip->dry_food_pickup-trip->other_supplies_pickup-trip->perishable_food_pickup);
+                int morefood = dist3(gen);
+                int d = ratio*morefood;
+                int p = r2*morefood;
+                int o = morefood-d-p;
+                Drop newdrop;
+                newdrop.dry_food = d;
+                newdrop.other_supplies = o;
+                newdrop.perishable_food = p;
+                newdrop.village_id = id;
+                trip->dry_food_pickup+=d;
+                trip->other_supplies_pickup+=o;
+                trip->perishable_food_pickup+=p;
+                trip->drops.push_back(newdrop);
+                prob1.d_max-=(d1+d2);
+                genn.pb({neigh,prob1});
             }
         }
     }
+    return genn;
 }
+
 Solution solve(const ProblemData& problem) {
     cout << "Starting solver..." << endl;
 
